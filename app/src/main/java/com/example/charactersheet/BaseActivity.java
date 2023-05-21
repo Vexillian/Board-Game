9b9c3b292a9b4085d5b763ee797fd724d68cbc4e
Vexillian <mrspike999@hotmail.com>
2023-05-21 15:33:38 -0500

    Stats Layout updated 4/15/23

--- app/src/main/java/com/example/charactersheet/BaseActivity.java
+++ app/src/main/java/com/example/charactersheet/BaseActivity.java
@@ -1,11 +1,13 @@
 package com.example.charactersheet;
 
+import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
